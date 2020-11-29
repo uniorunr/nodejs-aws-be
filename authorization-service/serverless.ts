060b2d8f0d199d63298842f13ce82b7096973206
uniorunr <gagael23@live.com>
2020-11-29 14:20:14 +0300

    feat: add config files for auth service

---
+++ authorization-service/serverless.ts
@@ -0,0 +1,27 @@
+import type { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: "authorization-service",
+  frameworkVersion: "2",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  plugins: ["serverless-webpack", "serverless-dotenv-plugin"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs12.x",
+    region: "${self:provider.environment.REGION}",
+    stage: "dev",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
